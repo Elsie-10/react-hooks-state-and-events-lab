62b580eaa3f974210f2b54d9a9d283ae787ea2aa
Elsie <elsieoscar06@gmail.com>
2025-04-11 11:14:53 +0300

    state and events

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+//import { useState } from "react/cjs/react.production.min";
 
 function App() {
+const [darkMode, setDarkMode] = useState(false); //create state
 
+const toggleDarkMode = () => {
+  setDarkMode(prevMode => !prevMode); // toggle mode
+};
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
-
+  
+const appClass = darkMode ? "App dark" : "App light"
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={toggleDarkMode}>
+          {darkMode ? "Light Mode" : "Dark Mode"}
+        </button>
       </header>
       <ShoppingList items={itemData} />
     </div>
+   
   );
 }
 
